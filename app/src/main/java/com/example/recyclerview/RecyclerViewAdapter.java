51ea0ffc9ea896fc7b9be3cb49511359336f140c
Cliff Gor <clierozigy@gmail.com>
2019-09-17 12:37:41 +0300

    Added a touast message and an OnClick method that displays the Dialog pop up

--- app/src/main/java/com/example/recyclerview/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/recyclerview/RecyclerViewAdapter.java
@@ -1,11 +1,14 @@
 package com.example.recyclerview;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,6 +19,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     Context mContext;
     List<Contact> mData;
+    Dialog myDialog;
 
     public RecyclerViewAdapter(Context mContext, List<Contact> mData) {
         this.mContext = mContext;
@@ -28,7 +32,32 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         View v ;
         v = LayoutInflater.from(mContext).inflate(R.layout.item_contact, parent, false );
-        MyViewHolder vHolder = new MyViewHolder(v);
+        final MyViewHolder vHolder = new MyViewHolder(v);
+
+
+
+        //Dialog ini
+
+        myDialog = new Dialog(mContext);
+        myDialog.setContentView(R.layout.dialog_contact);
+
+
+
+
+
+        vHolder.item_contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView dialog_name_tv = (TextView) myDialog.findViewById(R.id.dialog_name_id);
+                TextView dialog_phone_tv = (TextView) myDialog.findViewById(R.id.dialog_phone_id);
+                ImageView dialog_contact_image = (ImageView) myDialog.findViewById(R.id.dialog_img);
+                dialog_name_tv.setText(mData.get(vHolder.getAdapterPosition()).getName());
+                dialog_phone_tv.setText(mData.get(vHolder.getAdapterPosition()).getPhone());
+                dialog_contact_image.setImageResource(mData.get(vHolder.getAdapterPosition()).getPhoto());
+                Toast.makeText(mContext, "Test Click"+String.valueOf(vHolder.getAdapterPosition()), Toast.LENGTH_SHORT).show();
+                myDialog.show();
+            }
+        });
 
         return vHolder;
     }
@@ -50,6 +79,8 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
 
+
+        private LinearLayout item_contact;
         private TextView tv_name;
         private TextView tv_phone;
         private ImageView img;
@@ -60,7 +91,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             super(itemView);
 
 
-
+            item_contact = (LinearLayout) itemView.findViewById(R.id.contact_item_id);
             tv_name = (TextView) itemView.findViewById(R.id.name_contact);
             tv_phone = (TextView) itemView.findViewById(R.id.phone_contact);
             img = (ImageView) itemView.findViewById(R.id.image_contact);
